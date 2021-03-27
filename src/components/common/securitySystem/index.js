22d3ae08d962bc75bb5de332f5c03505dab77d2a
heptaluan <vd2o@foxmail.com>
2021-03-27 21:51:39 +0800

    add

--- src/components/common/securitySystem/index.js
+++ src/components/common/securitySystem/index.js
@@ -1,30 +1,20 @@
 import React from 'react'
 import './index.scss'
-import { Switch } from 'antd'
 
 const SecuritySystem = (props) => {
 
-  const onChange = (checked, id) => {
-    props.handleSwitchChange(checked, id)
-  }
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setDate(v => v = new Date())
+    }, 1000)
+    return() => clearInterval(id)
+  }, [])
 
   return (
-    <div className="security-system">
-      {
-        props.list.map(item => (
-          <div className={`control-box ${props.type === 'warn' ? 'img-box' : null}`} key={item.id}>
-            <h4>{item.title}</h4>
-            <>
-              {item.cover}
-            </>
-            {
-              item.type === 'switch'
-                ? <Switch defaultChecked={false} onChange={e => onChange(e, item.id)} />
-                : <p>警告</p>
-            }
-          </div>
-        ))
-      }
+    <div>
+      {date.toLocaleTimeString()}
     </div>
   )
 }
--- src/components/family/familyHeader/index.js
+++ src/components/family/familyHeader/index.js
@@ -1,10 +1,75 @@
 import React from 'react'
 import './index.scss'
 
-const FamilyHeader = (props) => {
+const FamilyHeader = props => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleOk = () => {
+    setIsModalVisible(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+
   return (
-    <div className="family-header">
-      123
+    <div className="chart-component">
+      <div className="box">
+        <h4 className="title">温度计</h4>
+        <div className="img-box">
+          <IconFont style={{ fontSize: '80px' }} type="icon-thermometer" />   
+        </div>
+        <p>温度：{props.data.temperature}</p>
+        <p>湿度：{props.data.humidity}</p>
+        <Button onClick={showModal} type="text">
+          图表展示
+        </Button>
+      </div>
+      <div className="box">
+        <h4 className="title">光照强度</h4>
+        <div className="img-box">
+          <IconFont style={{ fontSize: '80px' }} type="icon-icon-test" />
+        </div>
+        <p>光照：{props.data.illumination}</p>
+        <Button onClick={showModal} type="text">
+          图表展示
+        </Button>
+      </div>
+      <Modal visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <div className="chart-box">
+          <Recharts>
+            <Tooltip trigger="item" formatter="{a} : {c}" />
+            <Legend left="left" data={['温度', '湿度', '光照']} />
+            <XAxis
+              type="category"
+              data={props.data.chartData.xAxis}
+            >
+              <SplitLine show={false} />
+            </XAxis>
+            <Grid left="3%" right="4%" bottom="3%" containLabel={true} />
+            <YAxis type="log" minorSplitLine={{ show: true }} />
+            <Series
+              name="温度"
+              type="line"
+              data={props.data.chartData.temperature}
+            />
+            <Series
+              name="湿度"
+              type="line"
+              data={props.data.chartData.humidity}
+            />
+            <Series
+              name="光照"
+              type="line"
+              data={props.data.chartData.illumination}
+            />
+          </Recharts>
+        </div>
+      </Modal>
     </div>
   )
 }
