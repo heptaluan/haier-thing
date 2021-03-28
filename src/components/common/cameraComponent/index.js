f8afe85b02c852369822eb7254809611a8789403
heptaluan <vd2o@foxmail.com>
2021-03-28 20:09:09 +0800

    update

--- src/components/common/cameraComponent/index.js
+++ src/components/common/cameraComponent/index.js
@@ -3,10 +3,39 @@ import './index.scss'
 
 const CameraComponent = () => {
   return (
-    <div className="camera-component">
-      <div className="camera-box">
-       
-      </div>
+    <div className="family-menu">
+      <Menu
+        style={{ width: 200, paddingTop: 50 }}
+        mode="inline"
+        theme="light"
+        defaultSelectedKeys={['/family/home']}
+        selectedKeys={[history.location.pathname]}
+      >
+        <Menu.Item
+          key="/"
+          icon={<IconFont style={{ fontSize: '24px' }} type="icon-jiaju" />}
+        >
+          <Link to="/">首页</Link>
+        </Menu.Item>
+        <Menu.Item
+          key="/family/home"
+          icon={<IconFont style={{ fontSize: '24px' }} type="icon-jiaju" />}
+        >
+          <Link to="/family/home">智能家居</Link>
+        </Menu.Item>
+        <Menu.Item
+          key="/family/systems"
+          icon={<IconFont style={{ fontSize: '24px' }} type="icon-yuanqu" />}
+        >
+          <Link to="/family/systems">智能安防</Link>
+        </Menu.Item>
+        <Menu.Item
+          key="/family/entertainment"
+          icon={<IconFont style={{ fontSize: '24px' }} type="icon-jiaju" />}
+        >
+          <Link to="/family/entertainment">智能娱乐</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   )
 }
--- src/components/park/informationManagement/index.js
+++ src/components/park/informationManagement/index.js
@@ -6,42 +6,32 @@ import { Button } from 'antd'
 
 const InformationManagement = () => {
   return (
-    <div className="information-management">
-      <div className="information-box">
-        <div className="information-view">
-          <ul>
-            <li>
-              <span className="title">标签编号：</span>
-              <span className="information-content">--- --- ---</span>
-            </li>
-            <li>
-              <span className="title">车牌信息：</span>
-              <span className="information-content">鄂A-88888</span>
-            </li>
-            <li>
-              <span className="title">入场时间：</span>
-              <span className="information-content">--- --- ---</span>
-            </li>
-            <li>
-              <span className="title">出场时间：</span>
-              <span className="information-content">--- --- ---</span>
-            </li>
-            <li>
-              <span className="title">扣费信息：</span>
-              <span className="information-content">--- --- ---</span>
-            </li>
-          </ul>
+    <div className="intelligent-home-wrap">
+      <div className="intelligent-home">
+        <div className="chart-component-box">
+          <ChartComponent data={tempStatus} />
         </div>
-        <div className="information-input">
-          <h4>信息提示</h4>
-          <div className="btn-group">
-            <Button type="text">录入</Button>
-            <Button type="text">删除</Button>
-          </div>
+        <div className="warning-component-box">
+          <WarningComponent />
         </div>
-      </div>
-      <div className="information-cover">
-        <IconFont style={{ fontSize: '80px' }} type="icon-zidonglanganji" />
+        <div className="voice-component-box">
+          <h4>语音控制</h4>
+          <p>----</p>
+          <IconFont
+            style={{ fontSize: '100px' }}
+            type="icon-lvsefenkaicankaoxianban-"
+          />
+        </div>
+        <div className="control-component-box">
+          {states.map(item => (
+            <ControlComponent
+              key={item.id}
+              id={item.id}
+              data={item}
+              updateState={updateCurState}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
