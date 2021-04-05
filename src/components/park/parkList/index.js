27d12b8277ca72f8acd3fe6a702fc87beeb338a5
heptaluan <vd2o@foxmail.com>
2021-04-05 20:34:22 +0800

    update

--- src/components/park/parkList/index.js
+++ src/components/park/parkList/index.js
@@ -3,21 +3,38 @@ import './index.scss'
 import { Switch } from 'antd'
 
 const ParkList = props => {
-  const handleSwitchChange = checked => {
-    props.updateCurState(props.data.id, checked)
-  }
-
+  const [form] = Form.useForm()
   return (
-    <>
-      <div className="icon-box">
-        {(props.data.onIcon && props.data.id !== 5) || props.data.state
-          ? props.data.onIcon
-          : props.data.offIcon}
-      </div>
-      <div className="controls-switch">
-        <Switch defaultChecked={props.data.state} onChange={handleSwitchChange} />
-      </div>
-    </>
+    <Modal
+      visible={visible}
+      title="数据连接参数设置"
+      onCancel={onCancel}
+      okText="确定"
+      cancelText="取消"
+      onOk={() => {
+        form
+          .validateFields()
+          .then(values => {
+            form.resetFields()
+            onCreate(values)
+          })
+          .catch(info => {
+            console.log('Validate Failed:', info)
+          })
+      }}
+    >
+      <Form form={form} name="basic" initialValues={{ remember: true }}>
+        <Form.Item
+          label="数据服务地址"
+          name="address"
+          rules={[{ required: true, message: '请输入数据服务地址' }]}
+        >
+          <div>
+            <Input />
+          </div>
+        </Form.Item>
+      </Form>
+    </Modal>
   )
 }
 
