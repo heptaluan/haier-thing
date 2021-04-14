1ed3657ceffc6b020c56b52462efec61d727386e
heptaluan <vd2o@foxmail.com>
2021-04-14 20:07:36 +0800

    update

--- src/components/education/educationControl/index.js
+++ src/components/education/educationControl/index.js
@@ -1,96 +1,13 @@
 import React, { useState } from 'react'
 import './index.scss'
-import { Modal, Form, Input, message } from 'antd'
+import { message } from 'antd'
 import IconFont from '../../common/IconFont/index'
 import axios from 'axios'
-import { getRegisterUrl, getDeleteUrl, getUserToken } from '../../../api/api'
-
-const CollectionCreateForm = ({ visible, onCreate, onCancel, data }) => {
-  const [form] = Form.useForm()
-  return (
-    <Modal
-      visible={visible}
-      title="信息录入"
-      onCancel={onCancel}
-      okText="确定"
-      cancelText="取消"
-      onOk={() => {
-        form
-          .validateFields()
-          .then(values => {
-            form.resetFields()
-            onCreate(values)
-          })
-          .catch(info => {
-            console.log('Validate Failed:', info)
-          })
-      }}
-    >
-      <Form form={form} name="basic" initialValues={{ remember: true }}>
-        <Form.Item
-          label="课程名称"
-          name="courseName"
-          rules={[{ required: true, message: '请输入课程名称' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="开始时间"
-          name="startTime"
-          rules={[{ required: true, message: '请输入课程时间' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="结束时间"
-          name="endTime"
-          rules={[{ required: true, message: '请输入课程时间' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="教师姓名"
-          name="name"
-          rules={[{ required: true, message: '请输入教师姓名' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="学生人数"
-          name="studentCount"
-          rules={[{ required: true, message: '请输入学生人数' }]}
-        >
-          <Input />
-        </Form.Item>
-      </Form>
-    </Modal>
-  )
-}
+import { getDeleteUrl, getUserToken } from '../../../api/api'
 
 const EducationControl = props => {
   axios.defaults.headers.common['Authorization'] = getUserToken()
   const [visible, setVisible] = useState(false)
-
-  const onCreate = values => {
-    console.log(values)
-    const data = { ...values, cardId: props.data.cardId }
-    axios.post(getRegisterUrl(), {
-      type: 2,
-      value: {
-        ...data
-      },
-      cardId: props.data.cardId,
-    })
-    setVisible(false)
-  }
-
-  const handleRegister = data => {
-    if (data.cardId !== '-- --') {
-      setVisible(true)
-    } else {
-      message.error(`请先进行信息登记`);
-    }
-  }
 
   const handleDelete = data => {
     if (data.cardId !== '-- --') {
@@ -123,30 +40,12 @@ const EducationControl = props => {
           <span>结束时间：{props.data.startTime}</span>
         </li>
         <li>
-          <IconFont style={{ fontSize: '32px' }} type="icon-yonghuxinxi" />
-          <span>教师姓名：{props.data.name}</span>
-          <span className="edit" onClick={() => handleRegister(props.data)}>
-            课程录入
-          </span>
-          <span className="edit" onClick={() => handleDelete(props.data)}>
-            课程删除
-          </span>
-        </li>
-        <li>
           <span className="img-box">
             <IconFont style={{ fontSize: '32px' }} type="icon-yonghu" />
           </span>
           <span>学生人数：{props.data.studentCount}</span>
         </li>
       </ul>
-      <CollectionCreateForm
-        visible={visible}
-        onCreate={onCreate}
-        data={props.data}
-        onCancel={() => {
-          setVisible(false)
-        }}
-      />
     </div>
   )
 }
