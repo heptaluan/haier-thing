b66e2f117b9d742862eaaf7e444bd7627d67e976
heptaluan <vd2o@foxmail.com>
2021-04-16 20:39:19 +0800

    update

--- src/components/common/IconFont/index.js
+++
@@ -1,7 +0,0 @@
-import { createFromIconfontCN } from '@ant-design/icons'
-
-const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_2435701_x5q0hjp364.js',
-})
-
-export default IconFont
